ba88df56b6dde1d723cb627147ae4048ebd25ae8
EC2 Default User <ec2-user@ip-172-31-2-11.ap-south-1.compute.internal>
2018-02-10 08:48:09 +0000

    changed files

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,7 +12,7 @@ public class Greeter {
 
   }
 
-  public final String greet(final String someone) {
+  public final String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
